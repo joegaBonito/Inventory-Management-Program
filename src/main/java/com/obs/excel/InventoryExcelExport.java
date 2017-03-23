d1621a4ee7d8f9563547aaf8ac4112b26e2256ae
Joe Jung <joejung5678@gmail.com>
2017-03-23 13:50:22 -0400

    Added Master Inventory Excel Downloads

--- src/main/java/com/obs/excel/InventoryExcelExport.java
+++ src/main/java/com/obs/excel/InventoryExcelExport.java
@@ -25,6 +25,7 @@ public class InventoryExcelExport extends AbstractXlsView {
         List<Master> masters = (List<Master>) model.get("masters");
 
         // create excel xls sheet
+        Sheet sheet3 = workbook.createSheet("Master Inventory List");
         Sheet sheet1 = workbook.createSheet("Mobile Accessory Inventory List");
         Sheet sheet2 = workbook.createSheet("Unlocked Phone Inventory List");
         
@@ -88,6 +89,51 @@ public class InventoryExcelExport extends AbstractXlsView {
             courseRow.createCell(7).setCellValue(master.getPhone().getUnlockedPhonesInventory().getSalesQuantity());
             courseRow.createCell(8).setCellValue(master.getPhone().getUnlockedPhonesInventory().getCurrentInventoryAmount());
             courseRow.createCell(9).setCellValue(master.getPhone().getUnlockedPhonesInventory().getCurrentInventory());
+        	}
+        }
+        
+     // create sheet2 header row
+        Row header3 = sheet3.createRow(0);
+        header3.createCell(0).setCellValue("LG.COM Product ID");
+        header3.createCell(1).setCellValue("Product Name");
+        header3.createCell(2).setCellValue("Product Price");
+        header3.createCell(3).setCellValue("Sales Price");
+        header3.createCell(4).setCellValue("Purchased Amount");
+        header3.createCell(5).setCellValue("Purchased Quantity");
+        header3.createCell(6).setCellValue("Sales Amount");
+        header3.createCell(7).setCellValue("Sales Quantity");
+        header3.createCell(8).setCellValue("Inventory Amount");
+        header3.createCell(9).setCellValue("Current Inventory");
+        
+
+        // Create data cells for sheet2
+        int rowCount3 = 1;
+        for (Master master : masters){
+        	if(master.getPhone() == null) {
+                Row courseRow = sheet3.createRow(rowCount3++);
+                courseRow.createCell(0).setCellValue(master.getAccessory().getProductId());
+                courseRow.createCell(1).setCellValue(master.getAccessory().getProductName());
+                courseRow.createCell(2).setCellValue(master.getAccessory().getPurchasePrice());
+                courseRow.createCell(3).setCellValue(master.getAccessory().getSalesPrice());
+                courseRow.createCell(4).setCellValue(master.getAccessory().getAccessoryInventory().getTotalPurchasedAmount());
+                courseRow.createCell(5).setCellValue(master.getAccessory().getAccessoryInventory().getTotalPurchasedQuantity());
+                courseRow.createCell(6).setCellValue(master.getAccessory().getAccessoryInventory().getSalesAmount());
+                courseRow.createCell(7).setCellValue(master.getAccessory().getAccessoryInventory().getSalesQuantity());
+                courseRow.createCell(8).setCellValue(master.getAccessory().getAccessoryInventory().getCurrentInventoryAmount());
+                courseRow.createCell(9).setCellValue(master.getAccessory().getAccessoryInventory().getCurrentInventory());
+            	}
+        	if(master.getAccessory() == null) {
+            Row courseRow = sheet3.createRow(rowCount3++);
+           		courseRow.createCell(0).setCellValue(master.getPhone().getProductId());
+            	courseRow.createCell(1).setCellValue(master.getPhone().getProductName());
+            	courseRow.createCell(2).setCellValue(master.getPhone().getPurchasePrice());
+            	courseRow.createCell(3).setCellValue(master.getPhone().getSalesPrice());
+            	courseRow.createCell(4).setCellValue(master.getPhone().getUnlockedPhonesInventory().getTotalPurchasedAmount());
+            	courseRow.createCell(5).setCellValue(master.getPhone().getUnlockedPhonesInventory().getTotalPurchasedQuantity());
+            	courseRow.createCell(6).setCellValue(master.getPhone().getUnlockedPhonesInventory().getSalesAmount());
+            	courseRow.createCell(7).setCellValue(master.getPhone().getUnlockedPhonesInventory().getSalesQuantity());
+            	courseRow.createCell(8).setCellValue(master.getPhone().getUnlockedPhonesInventory().getCurrentInventoryAmount());
+            	courseRow.createCell(9).setCellValue(master.getPhone().getUnlockedPhonesInventory().getCurrentInventory());
         	}
         }
 	}
