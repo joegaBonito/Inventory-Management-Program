5fe8a32da099aaa3c977bc1e89a253374d50387f
YOONJAE JUNG/STAFF/LGEUS Americas Information Center(yoonjae.jung@lge.com) <yoonjae.jung@lge.com>
2017-05-25 16:40:18 -0400

    Error Handling function has been added. Check WebConfig.java file.

--- src/main/java/com/obs/General/controllers/HomeController.java
+++ src/main/java/com/obs/General/controllers/HomeController.java
@@ -3,9 +3,11 @@ package com.obs.General.controllers;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@ControllerAdvice
 public class HomeController {
 
 	@Secured({"ROLE_GUEST","ROLE_USER","ROLE_ADMIN"})
@@ -14,4 +16,16 @@ public class HomeController {
 		return "index";
 		
 	}
+	@RequestMapping("/error/error401")
+    public String error401() {
+        return "/error/error401";
+    }
+	@RequestMapping("/error/error404")
+    public String error404() {
+        return "/error/error404";
+    }
+	@RequestMapping("/error/error500")
+    public String error500() {
+        return "/error/error500";
+    }
 }
